02f103c5e8731504f8896e10fe19c553f841db8e
Melissa Perez <melmperz@gmail.com>
2023-03-09 15:35:35 -0500

    Add initial xml files

--- app/src/main/java/com/example/flixter/NowPlayingMoviesFragment.kt
+++ app/src/main/java/com/example/flixter/NowPlayingMoviesFragment.kt
@@ -2,7 +2,12 @@ package com.example.flixter
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val API_KEY = "5502e1574702c422bf438431d56a2184"
 
@@ -16,18 +21,18 @@ class NowPlayingMoviesFragment : Fragment(), OnListFragmentInteractionListener {
     }
 
     /*
-   * Constructing the view
-   */
+    * Constructing the view
+    */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_best_seller_books_list, container, false)
-        val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
-        val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
+        val view = inflater.inflate(R.layout.fragment_now_playing_movies_list, container, false)
+        //val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
+        //val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
         val context = view.context
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
-        updateAdapter(progressBar, recyclerView)
+        //recyclerView.layoutManager = GridLayoutManager(context, 2)
+        //updateAdapter(progressBar, recyclerView)
         return view
     }
 }
